a082532632b831098c4c4c1b837b5064a9503398
EC2 Default User <ec2-user@ip-172-31-90-156.ec2.internal>
2022-03-26 12:51:13 +0000

    Finish_kadai2

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,3 +2,5 @@ class HomesController < ApplicationController
   def top
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  
-  get 'books' => "books#index"
-  
-  post 'books' => "books#create" # /booksで保存するときはcreateアクション
-  
-  get 'books/:id' => "books#show", as: "book" # 'book'という名前のアクション
-  
-  get 'books/:id/edit' => 'books#edit', as: "edit_book"
-  
-  patch 'books/:id' => "books#update" , as: "update_book" # 更新
-  
-  delete 'books/:id' => "books#destroy", as: "destroy_book" # 削除
-  
-  get '/' => "homes#top"
+
+  root to: "homes#top"
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
